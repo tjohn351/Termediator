fb65368f0e9c4a999144757cb3fe473cef10f28d
tjohn351 <Tyler@Tylers-MacBook-Pro.local>
2016-02-23 21:45:12 -0700

    Patterns

--- Tests/timeSeries.py
+++ Tests/timeSeries.py
@@ -1,18 +1,24 @@
 from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+
+stopset = set(stopwords.words('english'))
 
 concept = 'A firewall is a part of a computer system or network that is designed to block unauthorized access while permitting authorized communications. It is a device or set of devices which is configured to permit or deny computer based application upon a set of rules and other criteria.'
 
-print len(concept)
-
 tokenizer = RegexpTokenizer(r'\w+')
-words = tokenizer.tokenize(concept)
+words = tokenizer.tokenize(concept.lower())
 
 pattern = ""
 
 for w in words:
-
-	if w 
-		pattern += "A"
-	else:
+	# print w
+	if w in stopset:
 		pattern += "B"
+	else:
+		pattern += "A"
+
+print pattern
+
+
+#http://stackoverflow.com/questions/20912364/remove-stopwords-and-tokenize-for-collocationbigramfinder-nltk
 	
